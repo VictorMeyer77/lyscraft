ed9f235fa32bfb680ed3ddbffe06d5a7408792dd
Victor-Algo <victor.algo.contact@gmail.com>
2023-07-20 21:53:53 +0200

    test gpiozero

--- src/main.py
+++ src/main.py
@@ -35,22 +35,54 @@ time.sleep(10)
 p.stop()
 GPIO.cleanup()"""
 
-from gpiozero import PWMOutputDevice
 import time
 
-ringer=PWMOutputDevice(12, initial_value=0,frequency=50)
+import RPi.GPIO as GPIO
 
-ringer.value = 0
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+p = GPIO.PWM(12, 50)
+
+p.start(0)
+print("Starting 0")
 time.sleep(5)
 
-ringer.value = 3.0 / 100.0
+p.ChangeDutyCycle(3)
+print("3")
 time.sleep(5)
 
+ii = 4
+steps = [5.85, 5.9, 5.95, 6.0]
+current_step = -1
 
-# fade in
-for duty_cycle in range(3, 10):
-    ringer.value = duty_cycle / 100.0
-    print(ringer.value)
-    time.sleep(0.1)
 
-time.sleep(5)
+def increase(i, target):
+    while i < target:
+        print(i)
+        p.ChangeDutyCycle(i)
+        time.sleep(0.05)
+        i += 0.02
+
+
+def decrease(i, target):
+    while i > target:
+        print(i)
+        p.ChangeDutyCycle(i)
+        time.sleep(0.05)
+        i -= 0.02
+
+
+while True:
+    s = input("commnd")
+    print(s)
+    if s == "a" and current_step < 3:
+        current_step += 1
+        increase(ii, steps[current_step])
+        ii = steps[current_step]
+    elif s == "z" and current_step > 0:
+        current_step -= 1
+        decrease(ii, steps[current_step])
+        ii = steps[current_step]
+    elif s == "p":
+        break
